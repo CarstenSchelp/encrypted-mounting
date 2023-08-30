bd3d8ed28c315ca1a8788ead577b428df996dee2
Carsten Schelp <cschelp@gmx.net>
2023-08-30 13:44:47 +0000

    py and ansible

--- py_and_ansible_upgrade.sh
+++ py_and_ansible_upgrade.sh
@@ -9,3 +9,6 @@ python3.9 --version
 
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1
 
+python3 -m pip install --upgrade --user ansible
+export PATH=~/.local/bin:$PATH
+
