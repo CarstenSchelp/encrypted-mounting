18e268d646216e5855abcd48f3ebe7b458df031b
Carsten Schelp <carsten.schelp@surf.nl>
2023-08-31 15:50:44 +0200

    check co name

--- py_and_ansible_upgrade.sh
+++ py_and_ansible_upgrade.sh
@@ -7,6 +7,8 @@ add-apt-repository -y ppa:deadsnakes/ppa
 apt install python3.9
 python3.9 --version
 
+apt install ansible
+
 # # update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1
 
 # python3 -m pip install --upgrade --user ansible
